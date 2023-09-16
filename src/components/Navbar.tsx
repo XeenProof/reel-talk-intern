c94b21282008a4aa90adba24e7ba10550bc30463
XeenProof <72842381+XeenProof@users.noreply.github.com>
2023-09-15 21:31:29 -0400

    pieces added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Navbar:React.FC = () => {
+    const handleClick = (text:string) => {
+        console.log(text);
+    }
+
+    return <div id='navbar'>
+        <div id='navbar-content'>
+            <div className='btn-group'>
+                <button className='btn' type='button' onClick={()=>{handleClick('Browse')}}>
+                    Browse
+                </button>
+                <button className='btn' type='button' onClick={()=>{handleClick('Community')}}>
+                    Community
+                </button>
+            </div>
+            <div className='right-inner-addon'>
+                <i className="icon-search"></i>
+                <input type="search" id="search" name="search" placeholder="Search.."/>
+            </div>
+            <div className='btn-group'>
+                <button className='btn' type='button' onClick={()=>{handleClick('Browse')}}>
+                    Browse
+                </button>
+            </div>
+        </div>
+    </div>
+}
+
+export default Navbar;
---
+++ src/components/ReelTalk.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+interface ReelTalkProps{
+
+}
+
+const ReelTalk:React.FC = (props:ReelTalkProps) => {
+    
+    
+    return (<div id='main-background'>
+        <Navbar/>
+    </div>);
+}
+
+export default ReelTalk;
