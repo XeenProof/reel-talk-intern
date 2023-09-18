0c106e0e2a8937230702cf530ba6512b9f13ef3d
XeenProof <72842381+XeenProof@users.noreply.github.com>
2023-09-18 13:40:19 -0400

    rename

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ProcessBarV2 from "./ProgressBar/ProgressBarV2";
 import BodyContent from "./BodyContent";
+import ProgressBarV3 from "./Progress/Progress";
 
 
 
 const Body:React.FC = () => {
     return <div id="body">
         <ProcessBarV2 current={5} total={6}/>
+        <ProgressBarV3/>
         <BodyContent/>
     </div>
 }
---
+++ src/components/body/Progress/Progress.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Progress:React.FC = () => {
+
+
+    return (<div id='process-bar-v3' className="center color1">
+        
+    </div>)
+}
+
+export default Progress;
