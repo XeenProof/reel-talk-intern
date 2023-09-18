83c7a58e6043343dc5b7687731aade852df8b52d
XeenProof <72842381+XeenProof@users.noreply.github.com>
2023-09-18 13:40:44 -0400

    rename 2

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ProcessBarV2 from "./ProgressBar/ProgressBarV2";
 import BodyContent from "./BodyContent";
-import ProgressBarV3 from "./Progress/Progress";
+import Progress from "./Progress/Progress";
 
 
 
 const Body:React.FC = () => {
     return <div id="body">
         <ProcessBarV2 current={5} total={6}/>
-        <ProgressBarV3/>
+        <Progress/>
         <BodyContent/>
     </div>
 }
