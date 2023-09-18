f3a7d2583d288154bd691a3da5a1b845d1fce65c
XeenProof <72842381+XeenProof@users.noreply.github.com>
2023-09-18 15:06:53 -0400

    ProgressBar V2 removed

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ProcessBarV2 from "./ProgressBar/ProgressBarV2";
 import BodyContent from "./BodyContent";
 import Progress from "./Progress/Progress";
 
@@ -7,7 +6,6 @@ import Progress from "./Progress/Progress";
 
 const Body:React.FC = () => {
     return <div id="body">
-        <ProcessBarV2 current={5} total={6}/>
         <Progress current={5} total={6}/>
         <BodyContent/>
     </div>
